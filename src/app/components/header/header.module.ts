055d1939f31210ee450b4e24224d5973e06989be
Tho Vo <votuongtho@gmail.com>
2024-09-11 21:24:47 +0200

    feat(header): add angular material and header content

--- src/app/components/header/header.module.ts
+++ src/app/components/header/header.module.ts
@@ -1,12 +1,14 @@
 import {NgModule} from '@angular/core';
 import {CommonModule} from '@angular/common';
 import {HeaderComponent} from "./header.component";
+import {RouterLink} from "@angular/router";
 
 
 @NgModule({
   declarations: [HeaderComponent],
   imports: [
-    CommonModule
+    CommonModule,
+    RouterLink
   ],
   exports: [HeaderComponent]
 })
